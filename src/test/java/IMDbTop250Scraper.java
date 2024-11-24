6ceb625500b7af32543055c4e7fb366a384393bb
Yevheniia Silenko <prencezza12@gmail.com>
2024-11-24 17:52:56 +0200

    separeted metods

--- src/test/java/IMDbTop250Scraper.java
+++ src/test/java/IMDbTop250Scraper.java
@@ -1,5 +1,5 @@
 import com.codeborne.selenide.*;
-import java.util.ArrayList;
+ import java.util.ArrayList;
 import java.util.List;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,6 +15,7 @@ public class IMDbTop250Scraper {
         double averageRating = calculateAverageRating(movieData);
         printAverageRating(averageRating);
     }
+
     private static void configureSelenide() {
         Configuration.timeout = 10000;
     }
@@ -28,6 +29,7 @@ public class IMDbTop250Scraper {
         SelenideElement top250Link = $("aside.drawer a:nth-child(2)");
         top250Link.should(Condition.exist).click();
     }
+
     private static List<String> scrapeMovies(int limit) {
         ElementsCollection movieElements = $$("li.ipc-metadata-list-summary-item");
         List<String> movieData = new ArrayList<>();
@@ -48,6 +50,7 @@ public class IMDbTop250Scraper {
 
         return movieData;
     }
+
     private static String extractMovieInfo(SelenideElement movieElement) {
         String title = movieElement.$("h3.ipc-title__text").getText();
         String yearText = movieElement.$("span:contains('(')").getText();
@@ -55,15 +58,18 @@ public class IMDbTop250Scraper {
         double rating = extractMovieRating(movieElement);
         return String.format("%s (%d) - %.1f", title, year, rating);
     }
+
     private static double extractMovieRating(SelenideElement movieElement) {
         String ratingText = movieElement.$("span.ipc-rating-star--rating").getText();
         return Double.parseDouble(ratingText);
     }
+
     private static void printTopMovies(List<String> movieData) {
         for (int i = 0; i < Math.min(5, movieData.size()); i++) {
             System.out.println(movieData.get(i));
         }
     }
+
     private static double calculateAverageRating(List<String> movieData) {
         List<Double> ratings = new ArrayList<>();
         for (String movie : movieData) {
@@ -78,6 +84,7 @@ public class IMDbTop250Scraper {
                 .average()
                 .orElse(0.0);
     }
+
     private static void printAverageRating(double averageRating) {
         System.out.println("Average movie rating: " + averageRating);
     }
