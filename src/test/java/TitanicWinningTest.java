55a258a0add6fde3ae050235fcee14f4d6cbed8e
Yevheniia Silenko <prencezza12@gmail.com>
2024-11-25 00:50:06 +0200

    Added Map for language and refreshed selectors for more universal

--- src/test/java/TitanicWinningTest.java
+++ src/test/java/TitanicWinningTest.java
@@ -2,19 +2,27 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
+import java.util.Map;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class TitanicWinningTest {
 
     private static final String TITANIC = "Titanic";
-    private static final String MOVIE_LINK = "a[href*='/title/tt0120338/']";
-    private static final String AWARDS_LINK = "a[href='/title/tt0120338/awards/?ref_=tt_awd']";
     private static final String MOVIE_YEAR = "1997";
+    private static final String AWARDS_SELECTOR = "[data-testid='awards']";
+
+    private static final Map<String, String> MOVIE_TITLES = Map.of(
+            "en", "Titanic",
+            "uk", "Титанік"
+    );
 
     @BeforeClass
     public void setUp() {
+        configureBrowser();
+    }
+
+    private void configureBrowser() {
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
         Configuration.browser = "chrome";
@@ -23,23 +31,32 @@ public class TitanicWinningTest {
     @Test
     public void testSearchForTitanic() {
         openIMDb();
-        searchForMovie();
-        clickOnMovieLink();
-        verifyPageHeader("Титанік");
+        searchForMovie(TITANIC);
+        clickOnMovieLink(MOVIE_YEAR);
+        verifyPageHeader(getLocalizedTitle());
         navigateToAwardsPage();
         verifyPageHeader("Awards");
     }
 
     private void openIMDb() {
-        open("https://www.imdb.com?language=en");
+        String language = getCurrentLanguage();
+        open("https://www.imdb.com?language=" + language);
     }
 
-    private void searchForMovie() {
-        $("input[name='q']").setValue(TITANIC).pressEnter();
+    private String getCurrentLanguage() {
+        return System.getProperty("user.language", "en");
     }
 
-    private void clickOnMovieLink() {
-        $(MOVIE_LINK).shouldBe(visible).click();
+    private void searchForMovie(String movieTitle) {
+        $("input[name='q']").setValue(movieTitle).pressEnter();
+    }
+
+    private void clickOnMovieLink(String movieYear) {
+        $$(".ipc-metadata-list-summary-item .ipc-metadata-list-summary-item__c")
+                .find(text(movieYear))
+                .shouldHave(text(getLocalizedTitle()))
+                .scrollIntoView(true) // Прокрутка до елементу
+                .click();
     }
 
     private void verifyPageHeader(String expectedTitle) {
@@ -47,8 +64,13 @@ public class TitanicWinningTest {
     }
 
     private void navigateToAwardsPage() {
-        SelenideElement awardsElement = $(AWARDS_LINK);
+        SelenideElement awardsElement = $(AWARDS_SELECTOR);
         awardsElement.scrollIntoView(true);
         awardsElement.shouldBe(visible, enabled).click();
+    }
+
+    private String getLocalizedTitle() {
+        String language = getCurrentLanguage();
+        return MOVIE_TITLES.getOrDefault(language, "Titanic");
     }
 }
