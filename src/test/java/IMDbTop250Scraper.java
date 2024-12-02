90f8552a8a297b7cd1724ac0a19cbd7cacabd122
Yevheniia Silenko <prencezza12@gmail.com>
2024-12-03 01:20:53 +0200

    This code works on my machine))

--- src/test/java/IMDbTop250Scraper.java
+++ src/test/java/IMDbTop250Scraper.java
@@ -5,65 +5,46 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class IMDbTop250Scraper {
     public static void main(String[] args) {
-        open("https://www.imdb.com");
+        openIMDbHomePage();
+        navigateToTop250();
 
-        SelenideElement burgerMenu = $("#imdbHeader-navDrawerOpen");
-        burgerMenu.shouldBe(visible).click();
-
-        SelenideElement top250Link = $("aside.drawer a:nth-child(2)");
-        top250Link.shouldBe(visible).click();
-
-        $$("ul > li h3").shouldBe(CollectionCondition.sizeGreaterThan(0));
-
-        List<SelenideElement> movieTitles = $$("ul > li h3");
-        System.out.println("Found " + movieTitles.size() + " movie titles.");
-
-        if (movieTitles.size() < 5) {
-            System.out.println("There are not enough movies available.");
-            return;
-        }
-
+        List<SelenideElement> movieTitles = $$("ul > li h3").shouldBe(CollectionCondition.sizeGreaterThan(0));
         double totalRating = 0;
-        int count = 0;
 
         for (int i = 0; i < 5; i++) {
             String title = movieTitles.get(i).getText();
+            String releaseYear = getReleaseYear(i);
+            double rating = getMovieRating(i);
 
-            SelenideElement releaseYearElement = $("ul > li:nth-child(" + (i + 1) + ") div.ipc-metadata-list-summary-item__c div div div.sc-300a8231-6.dBUjvq.cli-title-metadata > span:nth-child(1)");
-            releaseYearElement.shouldBe(visible);
-            String releaseYear = releaseYearElement.getText();
-
-            SelenideElement ratingElement = $("ul > li:nth-child(" + (i + 1) + ") div.ipc-metadata-list-summary-item__c > div > div > span > div");
-            ratingElement.shouldBe(visible);
-
-            String ratingText = ratingElement.getText();
-            if (ratingText.isEmpty()) {
-                System.out.println("Rating not found for movie " + (i + 1));
-                continue;
-            }
-
-            try {
-                ratingText = ratingText.replaceAll("[^0-9.]", "");
-                if (ratingText.indexOf('.') != ratingText.lastIndexOf('.')) {
-                    ratingText = ratingText.substring(0, ratingText.indexOf('.') + 3);
-                }
-
-                double rating = Double.parseDouble(ratingText);
-
+            if (rating != -1) {
                 totalRating += rating;
-                count++;
-
                 System.out.println("Movie " + (i + 1) + ": " + title + " (Release Year: " + releaseYear + ", Rating: " + rating + ")");
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid rating format for movie " + (i + 1) + ": " + ratingText);
             }
         }
 
-        if (count > 0) {
-            double averageRating = totalRating / count;
-            System.out.println("Average Rating of the Top 5 Movies: " + averageRating);
-        } else {
-            System.out.println("No ratings found for the top 5 movies.");
+        System.out.println(totalRating > 0 ? "Average Rating: " + totalRating / 5 : "No ratings found for top 5 movies.");
+    }
+
+    private static void openIMDbHomePage() {
+        open("https://www.imdb.com");
+    }
+
+    private static void navigateToTop250() {
+        $("#imdbHeader-navDrawerOpen").shouldBe(visible).click();
+        $("aside.drawer a:nth-child(2)").shouldBe(visible).click();
+        $("h1.ipc-title__text").shouldBe(visible); // Wait for page to load
+    }
+
+    private static String getReleaseYear(int index) {
+        return $("ul > li:nth-child(" + (index + 1) + ") div.ipc-metadata-list-summary-item__c div div div.sc-300a8231-6.dBUjvq.cli-title-metadata > span:nth-child(1)").getText();
+    }
+
+    private static double getMovieRating(int index) {
+        String ratingText = $("ul > li:nth-child(" + (index + 1) + ") div.ipc-metadata-list-summary-item__c > div > div > span > div").getText().replaceAll("[^0-9.]", "");
+        try {
+            return ratingText.isEmpty() ? -1 : Double.parseDouble(ratingText.substring(0, Math.min(ratingText.indexOf('.') + 3, ratingText.length())));
+        } catch (NumberFormatException e) {
+            return -1;
         }
     }
 }
