ba20949267e68c0ffff4f9f3fd9b275728ac055f
Yevheniia Silenko <prencezza12@gmail.com>
2024-12-02 23:43:20 +0200

    New task with new description)

--- src/test/java/IMDbTop250Scraper.java
+++ src/test/java/IMDbTop250Scraper.java
@@ -1,92 +1,69 @@
 import com.codeborne.selenide.*;
- import java.util.ArrayList;
 import java.util.List;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class IMDbTop250Scraper {
-
     public static void main(String[] args) {
-        configureSelenide();
-        openImdbPage();
-        openNavigationMenu();
-        navigateToTop250();
-        List<String> movieData = scrapeMovies(20);
-        printTopMovies(movieData);
-        double averageRating = calculateAverageRating(movieData);
-        printAverageRating(averageRating);
-    }
-
-    private static void configureSelenide() {
-        Configuration.timeout = 10000;
-    }
-    private static void openImdbPage() {
         open("https://www.imdb.com");
-    }
-    private static void openNavigationMenu() {
-        $("#imdbHeader-navDrawerOpen").should(Condition.exist).click();
-    }
-    private static void navigateToTop250() {
+
+        SelenideElement burgerMenu = $("#imdbHeader-navDrawerOpen");
+        burgerMenu.shouldBe(visible).click();
+
         SelenideElement top250Link = $("aside.drawer a:nth-child(2)");
-        top250Link.should(Condition.exist).click();
-    }
+        top250Link.shouldBe(visible).click();
 
-    private static List<String> scrapeMovies(int limit) {
-        ElementsCollection movieElements = $$("li.ipc-metadata-list-summary-item");
-        List<String> movieData = new ArrayList<>();
-        List<Double> ratings = new ArrayList<>();
+        $$("ul > li h3").shouldBe(CollectionCondition.sizeGreaterThan(0));
 
-        System.out.println("Found movies: " + movieElements.size());
-        movieElements.shouldHave(CollectionCondition.sizeGreaterThan(0));
+        List<SelenideElement> movieTitles = $$("ul > li h3");
+        System.out.println("Found " + movieTitles.size() + " movie titles.");
 
-        for (int i = 0; i < Math.min(limit, movieElements.size()); i++) {
+        if (movieTitles.size() < 5) {
+            System.out.println("There are not enough movies available.");
+            return;
+        }
+
+        double totalRating = 0;
+        int count = 0;
+
+        for (int i = 0; i < 5; i++) {
+            String title = movieTitles.get(i).getText();
+
+            SelenideElement releaseYearElement = $("ul > li:nth-child(" + (i + 1) + ") div.ipc-metadata-list-summary-item__c div div div.sc-300a8231-6.dBUjvq.cli-title-metadata > span:nth-child(1)");
+            releaseYearElement.shouldBe(visible);
+            String releaseYear = releaseYearElement.getText();
+
+            SelenideElement ratingElement = $("ul > li:nth-child(" + (i + 1) + ") div.ipc-metadata-list-summary-item__c > div > div > span > div");
+            ratingElement.shouldBe(visible);
+
+            String ratingText = ratingElement.getText();
+            if (ratingText.isEmpty()) {
+                System.out.println("Rating not found for movie " + (i + 1));
+                continue;
+            }
+
             try {
-                String movieInfo = extractMovieInfo(movieElements.get(i));
-                movieData.add(movieInfo);
-                ratings.add(extractMovieRating(movieElements.get(i)));
-            } catch (Exception e) {
-                System.out.println("Error processing movie data: " + e.getMessage());
+                ratingText = ratingText.replaceAll("[^0-9.]", "");
+                if (ratingText.indexOf('.') != ratingText.lastIndexOf('.')) {
+                    ratingText = ratingText.substring(0, ratingText.indexOf('.') + 3);
+                }
+
+                double rating = Double.parseDouble(ratingText);
+
+                totalRating += rating;
+                count++;
+
+                System.out.println("Movie " + (i + 1) + ": " + title + " (Release Year: " + releaseYear + ", Rating: " + rating + ")");
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid rating format for movie " + (i + 1) + ": " + ratingText);
             }
         }
 
-        return movieData;
-    }
-
-    private static String extractMovieInfo(SelenideElement movieElement) {
-        String title = movieElement.$("h3.ipc-title__text").getText();
-        String yearText = movieElement.$("span:contains('(')").getText();
-        int year = Integer.parseInt(yearText.replaceAll("[^\\d]", ""));
-        double rating = extractMovieRating(movieElement);
-        return String.format("%s (%d) - %.1f", title, year, rating);
-    }
-
-    private static double extractMovieRating(SelenideElement movieElement) {
-        String ratingText = movieElement.$("span.ipc-rating-star--rating").getText();
-        return Double.parseDouble(ratingText);
-    }
-
-    private static void printTopMovies(List<String> movieData) {
-        for (int i = 0; i < Math.min(5, movieData.size()); i++) {
-            System.out.println(movieData.get(i));
+        if (count > 0) {
+            double averageRating = totalRating / count;
+            System.out.println("Average Rating of the Top 5 Movies: " + averageRating);
+        } else {
+            System.out.println("No ratings found for the top 5 movies.");
         }
-    }
-
-    private static double calculateAverageRating(List<String> movieData) {
-        List<Double> ratings = new ArrayList<>();
-        for (String movie : movieData) {
-            try {
-                ratings.add(Double.parseDouble(movie.split(" - ")[1]));
-            } catch (Exception e) {
-                System.out.println("Error parsing rating: " + e.getMessage());
-            }
-        }
-        return ratings.stream()
-                .mapToDouble(Double::doubleValue)
-                .average()
-                .orElse(0.0);
-    }
-
-    private static void printAverageRating(double averageRating) {
-        System.out.println("Average movie rating: " + averageRating);
     }
 }
-
